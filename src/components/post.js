248fc525aeaa04e83d0bc9212c61dd01e2690ef9
Ashok <ashokbhatz@gmail.com>
2017-12-14 16:29:23 +0530

    Complete Project

---
+++ src/components/post.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class Post  extends Component{
+    render(){
+        return (
+        <div>
+            <h2></h2>
+            <p></p>
+        </div>
+        )
+    }
+}
+
+// export default function(prop){
+//     return(
+//         <div>
+//             <h2></h2>
+//             <p></p>
+//         </div>
+//     );
+// }
